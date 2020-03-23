db754063d13b0c073818f8213106dd9d91b3746f
mikeygit <biaogejiushibiao@outlook.com>
2020-03-23 15:43:59 +0800

    finish atm page design

--- src/view/ATM.vue
+++ src/view/ATM.vue
@@ -1,26 +1,47 @@
 <template>
-    <div>
+    <div id="main">
         <el-row :gutter="20">
-            <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="24">
+            <div class="grid-content bg-purple">
+                <div style="line-height: 50px">基于Web的自动取款机仿真系统</div>
+            </div>
+        </el-col>
         </el-row>
         <el-row :gutter="20">
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="16">
+                <div class="grid-content-hight bg-purple right-box-plus" style="text-align: center">
+                    <el-row><el-input type="textarea"  :rows="23" readonly="" placeholder="请选择业务 1:取款 2:存款 0:退出" v-model="textarea"></el-input></el-row>
+                </div>
+            </el-col>
+            <el-col :span="8">
+                <div class="grid-content-hight bg-purple right-box">
+                    <el-row style="padding: 10px"><el-input type="textarea" readonly="" :rows="8" placeholder="请输入内容" v-model="textarea"></el-input></el-row>
+                    <el-row><el-button>1</el-button><el-button>2</el-button><el-button>3</el-button></el-row>
+                    <el-row><el-button>4</el-button><el-button>5</el-button><el-button>6</el-button></el-row>
+                    <el-row><el-button>7</el-button><el-button>8</el-button><el-button>9</el-button></el-row>
+                    <el-row><el-button>确认</el-button><el-button>0</el-button><el-button>取消</el-button></el-row>
+                    <el-row><el-button style="width: 320px">打引凭条</el-button></el-row>
+                </div>
+            </el-col>
         </el-row>
         <el-row :gutter="20">
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="24">
+                <div class="grid-content bg-purple">
+                    <el-row style="line-height: 50px"><el-button>关机</el-button><el-button>插卡</el-button></el-row>
+                </div>
+            </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "ATM"
+        name: "ATM",
+        data() {
+            return {
+                textarea: ''
+            }
+        }
     }
 </script>
 
@@ -45,7 +66,24 @@
     }
     .grid-content {
         border-radius: 4px;
-        min-height: 36px;
+        min-height: 50px;
+    }
+    .grid-content-hight {
+        border-radius: 4px;
+        min-height: 500px;
+    }
+    .right-box {
+        padding: 10px;
+    }
+    .right-box-plus {
+        padding: 20px;
+        padding-bottom: 7px;
+    }
+    .right-box .el-button{
+        width: 100px;
+    }
+    >>> textarea {
+        text-align: center;
     }
     .row-bg {
         padding: 10px 0;
